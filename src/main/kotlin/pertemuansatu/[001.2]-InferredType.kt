a06bf2af7af418051581154c4325b83c4661707d
jutionck <jutionck@gmail.com>
2020-08-04 16:09:08 +0700

    Pertemuan satu

---
+++ src/main/kotlin/pertemuansatu/[001.2]-InferredType.kt
@@ -0,0 +1,16 @@
+package pertemuansatu
+
+fun main() {
+    val someString = "Some string"
+    val integerNumber = 75
+    val floatNumber = 3.5f
+    val doubleNumber = 3.141678686
+    val achar = 'f' // hanya bisa menerima 1 karakter
+
+    println(someString)
+    println(integerNumber)
+    println(floatNumber)
+    println(doubleNumber)
+    println(achar)
+    println(achar == 'f') // boolean
+}
