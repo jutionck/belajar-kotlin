70368200c600f7b0d8f377c1368c6185a3af30e1
jutionck <jutionck@gmail.com>
2020-08-07 10:12:40 +0700

    Refactor classs student

--- src/main/kotlin/pertemuanempat/Student.kt
+++ src/main/kotlin/pertemuanempat/Student.kt
@@ -2,14 +2,18 @@ package pertemuanempat
 
 class Student(val name: String, val age:Int, val gender:String) {
 
+    //override constructor, untuk mencetak value
     override fun toString(): String {
         return "Student(name='$name', age=$age, gender='$gender')"
     }
 
+    //override persamaan, perbandingan
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
+        //casting, si order ambil data dari class student
+        //S&K:
         other as Student
 
         if (name != other.name) return false
